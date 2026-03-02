475c46118d058b6fd9fb325748ea81dad608f69b
Leonard0SSilva <leonardosilva@Leonardos-Laptop.local>
2026-03-02 17:49:12 -0300

    Fix example installer

---
+++ Assets/Scripts/Infra/CloudModule/Runtime/Example/GameModulesExampleInstaller.cs
@@ -0,0 +1,21 @@
+﻿using Scaffold.Containers;
+using UnityEngine;
+
+namespace Scaffold.CloudModules.Example
+{
+    public class GameModulesExampleInstaller : Installer
+    {
+        public override void Install(IContainerRegistry registry, Transform holder)
+        {
+            GameModulesInstaller gameModulesInstaller = new GameModulesInstaller();
+            gameModulesInstaller.Install(registry, holder);
+            
+            registry.Register<SimpleController>(ContainerLifetime.Singleton)
+                .AsImplementedInterfaces();
+            registry.Register<CounterController>(ContainerLifetime.Singleton)
+                .AsImplementedInterfaces();
+            registry.Register<ReactiveCounterController>(ContainerLifetime.Singleton)
+                .AsImplementedInterfaces();
+        }
+    }
+}
