0d7d62696943129d9ac8eb77f4fe1d3c8ae51442
Leonard0SSilva <s2leoj@gmail.com>
2026-07-24 06:44:38 -0300

    fix(analytics): support Unity 6000.5 event recording

--- Assets/Packages/com.scaffold.analytics/Runtime/Implementation/AnalyticsService.cs
+++ Assets/Packages/com.scaffold.analytics/Runtime/Implementation/AnalyticsService.cs
@@ -1,3 +1,7 @@
+#if UNITY_6000_5_OR_NEWER
+using System.Collections.Generic;
+using Unity.Services.Analytics;
+#endif
 using UnityEngine;
 using VContainer.Unity;
 using UGSAnalyticsService = Unity.Services.Analytics.AnalyticsService;
@@ -17,10 +21,20 @@ namespace Scaffold.Analytics
         public void Record<T>(T evt) where T : AnalyticsEvent
         {
             Debug.Log($"[AnalyticsService] Sending event of type '{typeof(T).FullName}'.");
-            
+
             if (UGSAnalyticsService.Instance != null)
             {
+#if UNITY_6000_5_OR_NEWER
+                CustomEvent customEvent = new CustomEvent(evt.Name);
+                foreach (KeyValuePair<string, object> parameter in evt.Parameters)
+                {
+                    customEvent.Add(parameter.Key, parameter.Value);
+                }
+
+                UGSAnalyticsService.Instance.RecordEvent(customEvent);
+#else
                 UGSAnalyticsService.Instance.CustomData(evt.Name, evt.Parameters);
+#endif
             }
         }
     }
